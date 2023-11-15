75b9a4fd86f1e4672ba7194bd01190625c8517a5
kayode <kayode.ayelagbe@gmail.com>
2023-11-15 03:43:28 +0100

    first commit

---
+++ react-notes-sep-main/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import About from './component/About';
+import Home from './component/Home';
+function App() {
+  return (
+    <div className="App">
+      <About />
+      <Home />
+      
+    </div>
+  );
+}
+ 
+export default App;
---
+++ react-notes-sep-main/src/component/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+class About extends Component {
+  render() {
+    return (
+     <h1>This is a class component</h1>
+    )
+  }
+}
+
+export default About
+
+// rafce - functional component
+// rce - class component
